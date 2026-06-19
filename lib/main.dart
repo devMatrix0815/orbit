2e4a8d21a8751f3262618079ca22c0514d123615
LeoVier <leoweitlaner@gmail.com>
2026-06-19 18:57:37 +0200

    clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+
+// packages
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+// firebase options
 import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'screens/main_screen.dart';
 import 'screens/setup_profile_screen.dart';
 
 void main() async {
+  // for async functions
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -22,6 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
       // light theme
       theme: ThemeData(
         useMaterial3: true,
@@ -33,6 +39,7 @@ class MyApp extends StatelessWidget {
           outline: Color(0xFF555555),
         ),
       ),
+
       // dark theme
       darkTheme: ThemeData(
         useMaterial3: true,
@@ -44,15 +51,21 @@ class MyApp extends StatelessWidget {
           outline: Color(0xFFB8B8B8),
         ),
       ),
+
+      // set thememode
       themeMode: ThemeMode.light,
+
+      // homepage
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
+          // loading before data
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Scaffold(
               body: Center(child: CircularProgressIndicator()),
             );
           }
+
           if (snapshot.hasData) {
             return FutureBuilder<DocumentSnapshot>(
               future: FirebaseFirestore.instance
@@ -60,20 +73,26 @@ class MyApp extends StatelessWidget {
                   .doc(snapshot.data!.uid)
                   .get(),
               builder: (context, docSnapshot) {
+                // loading
                 if (docSnapshot.connectionState == ConnectionState.waiting) {
                   return const Scaffold(
                     body: Center(child: CircularProgressIndicator()),
                   );
                 }
-                final data =
-                    docSnapshot.data?.data() as Map<String, dynamic>?;
+
+                // main screen
+                final data = docSnapshot.data?.data() as Map<String, dynamic>?;
                 if (data != null && data['profileComplete'] == true) {
                   return const MainScreen();
                 }
+
+                // Setup profile site
                 return const SetupProfileScreen();
               },
             );
           }
+
+          // login screen
           return const LoginScreen();
         },
       ),
